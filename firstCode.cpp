e0b471562722fd27c7280afa6944c5de594891f2
Shahzaneer Ahmed <shahism01@gmail.com>
2022-01-25 02:01:28 +0500

    initial commit

---
+++ firstCode.cpp
@@ -0,0 +1,9 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Pakistan Noor hai aur noor ko zawaal nhi! ~ Wasif Ali Wasif\n";
+    std::cout <<"Shahzaneer Ahmed taking on cpp";
+    return 0;
+}
