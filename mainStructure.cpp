1d4ae0dd062d1d3805dd55846420c7f408d8f51b
Shahzaneer Ahmed <shahism01@gmail.com>
2022-01-25 03:02:42 +0500

    main structure

---
+++ mainStructure.cpp
@@ -0,0 +1,14 @@
+// header files are included with it
+#include <iostream>
+using namespace std;
+// main method is the entry point of the program
+int main(){
+// cout << is used for printing 
+// cin >> is used for taking input 
+// endl means end the line
+cout<<"Enter your marks"<<endl;
+int marks; //declaring the variable marks
+cin>>marks; //taking input via this
+
+cout<<marks<<endl;
+}
