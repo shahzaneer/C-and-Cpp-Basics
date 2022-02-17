4e07eec307330dab95cb8c3adbc39c1657ff162a
Shahzaneer Ahmed <shahism01@gmail.com>
2022-02-17 23:34:05 +0500

    enum added

---
+++ enum1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// ! enumeration (enum) -- > a data type to give some constant values 
+// ! mostly used in flags
+enum week {mon,tues,wednes,thurs,fri,satur,sun};
+enum evens {
+    shah = 2,
+    kaleem = 4,
+    noor = 6,
+    rabiya = 8
+    //?  there is no semicolon in the body of enum
+};
+
+int main(){
+    // mon will get 0 value tues will get 1 and then so on!
+    cout<<wednes;
+
+    cout<<rabiya;
+
+    return 0;
+}
