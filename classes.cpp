f7989e0824d080c65d3d919c87650bf6b6912258
Shahzaneer Ahmed <shahism01@gmail.com>
2022-02-19 00:00:41 +0500

    class added

--- classes.cpp
+++ classes.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 class complex{
     // private: k bad jo bhi cheez declare karengay wo private hogi and so on...
-
+// class k ander bnne walay function by default inline houtay hain aur bahir walay normal houtay hain
 private:
     int a,b;    
 public:
@@ -20,12 +20,29 @@ public:
     void show_data(){
         cout<<"\n"<<a<<" "<<b;
     }
+    void shahzaneerAhmed(); //declared the prototype
+
+
+// we can also define a function outside the class but we have to declare the 
+// prototype of that function in the same class!
 
 };
+
+
+// ab class se bahir jaker will define the function
+// lekin isko link krne k liay return type aur name k darmiyan membership operator lgana hoga
+// classname ::
+void complex:: shahzaneerAhmed(){
+        cout<<"\nPakistan noor hai aur noor ko zawaal nhi!";
+    }
 int main(){
     complex c1;
+    // c1 aik object hai simple variable nhi 
+    // (same object class ka concept jo ap pichle 1 saal se parh rhay hain)
+
     c1.set_data(3,4);
     c1.show_data();
+    c1.shahzaneerAhmed();
 
     return 0;
 }
