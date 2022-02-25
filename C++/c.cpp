2612be98d14299d79095c713146d8d0cb0ce1180
Shahzaneer Ahmed <shahism01@gmail.com>
2022-02-25 16:19:23 +0500

    c program

---
+++ C++/c.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(){
+printf("stdio.h is used in cpp\n");
+std::cout<<"Hello world";
+    return 0;
+}
---
+++ C/print.c
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+
+    std::cout<<"Shahzaneer Ahmed taking on C";
+    return 0;
+
+    // It will give errors because C cannot used preprocessors
+    // of its successor language C++.
+    // although cpp can use c things like structure, stdio.h etc type things
+
+}
